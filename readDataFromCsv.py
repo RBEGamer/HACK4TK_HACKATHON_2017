1ed3f2b52957f19c2a79de6d98d93ddb88d12471
RBEGamer <marcel.ochsendorf@gmail.com>
2017-06-26 23:05:32 +0200

    added working python script

--- readDataFromCsv.py
+++ readDataFromCsv.py
@@ -1,16 +1,23 @@
 import csv
 import time
-import request
+import requests
 
-with open('main.csv', 'rb') as f:
-
-    reader = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
-    for row in reader:
-        read = request.get(109.230.230.209/hack4tk/insert_data.php?token=tk4hack&comp_id=2&mach_id=1&value=10&suid=tempSensor&type=temp)
-        print row
-        read.txt
-        time.sleep(10)
-        read = request.get(109.230.230.209/hack4tk/insert_data.php?token=tk4hack&comp_id=2&mach_id=1&value=12&suid=humSensor&type=hum)
-        print row
-        read.txt
-        time.sleep(1000)
+print("DATA SIMULATOR STARTED MAINOUT.CSV")
+while 1:
+    with open('./mainout.csv', 'rt') as f:
+        reader = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+        row_counter = 0;
+        for row in reader:
+            if row_counter == 0:
+                row_counter = 1
+                continue
+            read = requests.get("http://109.230.230.209/hack4tk/insert_data.php?token=tk4hack&comp_id=2&mach_id=1&value="+ row[2] +"&suid=tempSensor&type=temp")
+            print(read.text)
+            print(row[0])
+                #read.text
+            time.sleep(10)
+            read = requests.get("http://109.230.230.209/hack4tk/insert_data.php?token=tk4hack&comp_id=2&mach_id=1&value="+ row[3] +"&suid=humSensor&type=hum")
+            print(read.text)
+                #print(row)
+            read.text
+            time.sleep(2000)
