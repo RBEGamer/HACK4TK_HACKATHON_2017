77f1c4e2a0826919de0a73853980ea8840301610
DiagnoseSchmerz <diagnoseschmerz@web.de>
2017-06-26 21:31:54 +0200

    Update serverConnectRead.py
    Changed the connectionToServer part, we just have to access on server yet

--- serverConnectRead.py
+++ serverConnectRead.py
@@ -2,13 +2,12 @@ import OpenOPC
 
 string itemList
 
-#search for avaialble senors
-opc.servers()
-['Matrikon.OPC.Simulation.1', 'Kepware.KEPServerEX.V4']
+#search for avaialble senors, which is not needed yet, there's only one server available
+#opc.servers()
+#['Matrikon.OPC.Simulation.1', 'Kepware.KEPServerEX.V4']
 
 #connecting to server
-'opc = OpenOPC.open_client('localhost')
-'opc.connect('ourServer')
+opc.connect('ourServer')
 
 #which items are available
 opc.list('itemList')
