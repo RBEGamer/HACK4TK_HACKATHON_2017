fb94dfb2dc8391f1ed10454d963c7e17a3a4486c
DiagnoseSchmerz <diagnoseschmerz@web.de>
2017-06-27 05:48:56 +0200

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,8 +1,12 @@
 import json
 import requests
+from gtts import gTTS
 read = requests.get("http://109.230.230.209/hack4tk/get_warning_message.php?token=tk4hack")
 if (read != '[]'):
     read.text
 js = '[{0}]'.format(read.text)
-json.loads(js)
-print (js)
+data = json.loads(js)
+
+print(data)
+
+tts = gTTS(text='Temperature too high please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=True)
