65c3f8368f91736c8d0255bdf5cae6524da18922
DiagnoseSchmerz <diagnoseschmerz@web.de>
2017-06-27 06:36:12 +0200

    Update parser.py
    Now with improved gTTS and push notification!

--- parser.py
+++ parser.py
@@ -2,6 +2,8 @@ import json
 import requests
 from gtts import gTTS
 import os
+from pushover import init, Client
+
 read = requests.get("http://109.230.230.209/hack4tk/get_warning_message.php?token=tk4hack")
 if (read != '[]'):
     read.text
@@ -10,7 +12,11 @@ data = json.loads(js)
 
 print(data)
 
-tts = gTTS(text='Temperature too high please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=False)
+tts = gTTS(text='Temperature too high in '+data[6]["name"]' please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=False)
 tts.save("temp.mp3")
 os.system("vlc temp.mp3")
+
+init("<token>")
+Client("<user-key>").send_message('Please contact '+data[0]["contact_name"]+' under '+data[12]["3"]' ', title="Warning Message")
+
 
