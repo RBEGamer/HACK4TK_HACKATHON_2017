2e840a32865905a5cfe97cbc84cbda0a4fc70a19
notagenius <shiplost@126.com>
2017-06-27 11:34:32 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,20 +1,23 @@
 import json
 import requests
 from gtts import gTTS
+import time
 import os
 from pushover import init, Client
 
-read = requests.get("http://109.230.230.209/hack4tk/get_warning_message.php?token=tk4hack")
-if ('{' in read):
-    read.text
-    print (read.text)
-    init("aimoegrsmp6xhz4mxgx5c16p7qkkcj")
-    js = '[{0}]'.format(read.text)
-    data = json.loads(js)
 
-    print(data)
-    while(True):
-        tts = gTTS(text='Temperature too high in '+data[6]["name"]+' please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=False)
+
+while(True):
+    read = requests.get("http://109.230.230.209/hack4tk/get_warning_message.php?token=tk4hack")
+    cont = read.text
+    print(cont)
+    if ('{' in cont):
+
+        init("aimoegrsmp6xhz4mxgx5c16p7qkkcj")
+        js = '[{0}]'.format(cont)
+        data = json.loads(js)
+        tts = gTTS(text='Temperature too high in vertical mill please contact Dr.-Ing Heinrich Hiesinger imediatly', lang='en-us', slow=False)
         tts.save("temp.mp3")
         os.system("vlc temp.mp3")
-        Client("ucndiysyovk86rgy1n16dv9pryzm54").send_message('Please contact '+data[0]["contact_name"]+' under '+data[12]["3"]+' 00492018440', title="Warning Message")
+        Client("ucndiysyovk86rgy1n16dv9pryzm54").send_message('Please contact Dr.-Ing Heinrich Hiesinger under  00492018440', title="Warning Message")
+        time.sleep(10)
