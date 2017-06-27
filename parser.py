ade281f512b3f281b427a039134e6196af2834bb
DiagnoseSchmerz <diagnoseschmerz@web.de>
2017-06-27 08:51:16 +0200

    Update parser.py
    Now with actual function(?)

--- parser.py
+++ parser.py
@@ -5,17 +5,16 @@ import os
 from pushover import init, Client
 
 read = requests.get("http://109.230.230.209/hack4tk/get_warning_message.php?token=tk4hack")
-if (read != '[]'):
+if ('{' in read):
     read.text
-js = '[{0}]'.format(read.text)
-data = json.loads(js)
-print(data)
-while(True):
-    tts = gTTS(text='Temperature too high in Vertikal Muehle please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=False)
-    tts.save("temp.mp3")
-    os.system("vlc temp.mp3")
-
+    print (read.text)
     init("aimoegrsmp6xhz4mxgx5c16p7qkkcj")
-    Client("ucndiysyovk86rgy1n16dv9pryzm54").send_message('Please contact '+data[0]["contact_name"]+' under 00492018440', title="Warning Message")
+    js = '[{0}]'.format(read.text)
+    data = json.loads(js)
 
-
+    print(data)
+    while(True):
+        tts = gTTS(text='Temperature too high in '+data[6]["name"]+' please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=False)
+        tts.save("temp.mp3")
+        os.system("vlc temp.mp3")
+        Client("ucndiysyovk86rgy1n16dv9pryzm54").send_message('Please contact '+data[0]["contact_name"]+' under '+data[12]["3"]+' 00492018440', title="Warning Message")
