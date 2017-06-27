d14018f539064c70eb8d0447aba350ea9558e9b1
notagenius <shiplost@126.com>
2017-06-27 06:19:44 +0200

    functional!

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from gtts import gTTS
+import os
 read = requests.get("http://109.230.230.209/hack4tk/get_warning_message.php?token=tk4hack")
 if (read != '[]'):
     read.text
@@ -9,4 +10,7 @@ data = json.loads(js)
 
 print(data)
 
-tts = gTTS(text='Temperature too high please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=True)
+tts = gTTS(text='Temperature too high please contact '+data[0]["contact_name"]+' imediatly', lang='en', slow=False)
+tts.save("temp.mp3")
+os.system("vlc temp.mp3")
+
